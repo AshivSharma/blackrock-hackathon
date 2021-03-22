d77426561e5d9bef6c8d606e94a9c949507d09d2
Munia Rahman <munia630@gmail.com>
2021-03-22 01:41:47 -0400

    Stockpage initial work

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-export const HOMEPAGE = "/homepage";
-export const INDUSTRY_TABLE = "/industrytable";
+export const HOMEPAGE = "/home";
+export const INDUSTRY_TABLE = "/table";
 export const STOCK = "/s/:ticker";
 export const NOT_FOUND = "/not-found";
