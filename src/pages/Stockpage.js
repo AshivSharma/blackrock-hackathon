9264dbfe42b419d8e6c57ea273839565966cedff
Munia Rahman <munia630@gmail.com>
2021-03-22 01:51:33 -0400

    Testing

--- src/pages/Stockpage.js
+++ src/pages/Stockpage.js
@@ -53,6 +53,22 @@ export default function Stockpage() {
           of any language, paragraphs are usually an expected part of formal
           writing, used to organize longer prose.
         </Box>
+        <Box
+          display="flex"
+          alignItems="flex-center"
+          m={20}
+          bgcolor="background.paper"
+          fontSize={15}
+          fontWeight={100}
+          fontFamily="monospace"
+          css={{ height: 150, width: "80%" }}
+        >
+          <GridInsideFlexbox />A paragraph is a self-contained unit of discourse
+          in writing dealing with a particular point or idea. A paragraph
+          consists of one or more sentences. Though not required by the syntax
+          of any language, paragraphs are usually an expected part of formal
+          writing, used to organize longer prose.
+        </Box>
       </div>
     </div>
   );
